a8403b398c6054e013fcfde9f700c917709486b5
Bijoin Krishna bairagi <bijon.bairagi@gmail.com>
2024-01-15 18:23:10 +0600

    ADD PLACE FORM DONE

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,8 +1,107 @@
-import React from 'react'
+import React, { useCallback, useReducer } from 'react'
+import Input from '../../shared/components/ui/Input'
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators'
+
+
+const formReducer = (state, action) => {
+    switch (action.type) {
+        case 'INPUT_CHANGE':
+            let formIsValid = true;
+            for (const inputId in state.inputs) {
+                if (inputId === action.inputs)
+                    formIsValid = formIsValid && action.isValid;
+                else {
+                    formIsValid = formIsValid && state.inputs[inputId].isValid;
+                }
+            }
+            return {
+                ...state,
+                inputs: {
+                    ...state.inputs,
+                    [action.inputId]: { value: action.value, isValid: action.isValid }
+                },
+                isValid: formIsValid
+            };
+        default:
+            return state;
+    }
+}
 
 const NewPlace = () => {
+    const [formState, dispatch] = useReducer(formReducer, {
+        inputs: {
+            title: {
+                value: '',
+                isValid: false
+            },
+            description: {
+                value: '',
+                isValid: false
+            },
+            address: {
+                value: '',
+                isValid: false
+            }
+        },
+        isValid: false
+    });
+    const inputHandeler = useCallback((id, value, isValid) => {
+        dispatch({ type: 'INPUT_CHANGE', value: value, isValid: isValid, inputId: id })
+    }, []);
+
+
+
+    const addBtn = !formState.isValid ? (
+        <button type='submit' disabled className="text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add Place</button>
+    ) : (
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Place</button>
+    );
+
+    const placeSubmitHandeler = event => {
+        event.preventDefault();
+
+        console.log(formState.inputs);
+    }
+
+
     return (
-        <div>NewPlace</div>
+        <div className='flex flex-col items-center'>
+            <form onSubmit={placeSubmitHandeler}>
+                <Input
+                    id="title"
+                    element="input"
+                    type="text"
+                    label="Title"
+                    validators={[VALIDATOR_REQUIRE()]}
+                    onInput={inputHandeler}
+                    placeholder="Enter yout Title"
+                    errorText="Please enter a valid title."
+                />
+                <Input
+                    id="description"
+                    element="textarea"
+                    type="text"
+                    rows="3"
+                    label="Description"
+                    validators={[VALIDATOR_MINLENGTH(5)]}
+                    onInput={inputHandeler}
+                    placeholder="Enter yout Title"
+                    errorText="Please enter a valid sescription (at last 5 character)."
+                />
+                <Input
+                    id="address"
+                    element="input"
+                    type="text"
+                    rows="3"
+                    label="Addrtess"
+                    validators={[VALIDATOR_REQUIRE()]}
+                    onInput={inputHandeler}
+                    placeholder="Enter yout Title"
+                    errorText="Please enter a valid Address."
+                />
+                {addBtn}
+            </form>
+        </div>
     )
 }
 
---
+++ src/shared/components/ui/Input.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useReducer } from 'react'
+import { validate } from '../../util/validators'
+
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE':
+            return {
+                ...state,
+                value: action.val,
+                isValid: validate(action.val, action.validators)
+            };
+        case 'TOUCH': {
+            return {
+                ...state,
+                isTouched: true
+            }
+        }
+        default:
+            return state;
+    }
+};
+
+const Input = props => {
+    const [inputState, dispatch] = useReducer(inputReducer, {
+        value: '',
+        isTouched: false,
+        isValid: false,
+    });
+    const { id, onInput } = props;
+    const { value, isValid } = inputState;
+    useEffect(() => {
+        onInput(id, value, isValid)
+    }, [id, onInput, value, isValid])
+
+    const changeHandler = event => {
+        dispatch({ type: 'CHANGE', val: event.target.value, validators: props.validators });
+    };
+
+    const touchHandler = () => {
+        dispatch({
+            type: 'TOUCH'
+        });
+    };
+
+    const element = props.element === 'input' ? (
+        <input
+            id={props.id}
+            type={props.type}
+            placeholder={props.placeholder}
+            onChange={changeHandler}
+            onBlur={touchHandler}
+            value={inputState.value}
+            className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${!inputState.isValid && 'border border-red-500 text-red-900 placeholder-red-700'} }`} />
+    ) : (
+        <textarea
+            id={props.id}
+            rows={props.rows || 3}
+            onChange={changeHandler}
+            value={inputState.value}
+            onBlur={touchHandler}
+            className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  ${!inputState.isValid ? ' border-red-500 text-red-900 placeholder-red-700' : ''}`} />
+    );
+
+
+
+    return (
+        <div className='min-w-96'>
+            <label htmlFor={props.id} className={`block mb-2 text-sm font-medium text-gray-900 dark:text-white ${!inputState.isValid && 'text-red-500'}`} >{props.label}</label>
+            {element}
+            {!inputState.isValid && <p className='text-red-500'>{props.errorText}</p>}
+        </div>
+    )
+}
+
+export default Input
