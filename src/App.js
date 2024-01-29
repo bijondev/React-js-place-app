1d87b45bcbd72a8d8c0cbdebcea2d2d1ca85e235
Bijoin Krishna bairagi <bijon.bairagi@gmail.com>
2024-01-29 12:25:54 +0600

    token expire added

--- src/App.js
+++ src/App.js
@@ -14,10 +14,15 @@ function App() {
   const [token, setToken] = useState('');
   const [userId, setUserId] = useState(null);
 
-  const login = useCallback((uId, token) => {
+  const login = useCallback((uId, token, exprationDate) => {
     setToken(token);
     setUserId(uId);
-    localStorage.setItem('userData', JSON.stringify({ userId: uId, token: token }))
+    const tokenExpirationDate = exprationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    localStorage.setItem('userData', JSON.stringify({
+      userId: uId,
+      token: token,
+      tokenExpirationDate: tokenExpirationDate.toISOString()
+    }))
   }, []);
 
   const logout = useCallback(() => {
@@ -28,8 +33,10 @@ function App() {
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem('userData'));
-    if (storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
+    if (storedData &&
+      storedData.token &&
+      new Date(storedData.tokenExpirationDate) > new Date()) {
+      login(storedData.userId, storedData.token, new Date(storedData.tokenExpirationDate));
     }
   }, [login]);
 
